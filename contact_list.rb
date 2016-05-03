cc06f2aacb02b280200f586b2ac1807a8821f07e
Ryan Callaway <callaway.r@gmail.com>
2016-05-03 12:43:22 -0700

    implemented main menu, user input, list command, new command

--- contact_list.rb
+++ contact_list.rb
@@ -5,5 +5,35 @@ require_relative 'contact'
 class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  if ARGV.empty?
+    puts "Here is a list of available commands:\n
+        new    - Create a new contact\n
+        list   - List all contacts\n
+        show   - Show a contact\n
+        search - Search contacts"
+    return
+  end
 
+  case ARGV[0]
+    when "list"
+      num_contacts = 0
+      CSV.foreach('contacts.csv') do |row|
+        num_contacts += 1
+        puts "#{num_contacts}: "+row[0]+" "+row[1]+"\n"
+      end
+      num_contacts = 
+      puts "--\n#{num_contacts} records total"
+    when "new"
+      puts "Full name?"
+      name = STDIN.gets.chomp
+      puts "Email address?"
+      email = STDIN.gets.chomp
+      new_contact = [name,email]
+      new_contact = new_contact.to_csv
+      File.open("contacts.csv", "a+") do |io| 
+        io.puts()
+        io.puts(new_contact)
+      end
+      puts "New contact (#{name}) saved successfully"
+  end
 end
